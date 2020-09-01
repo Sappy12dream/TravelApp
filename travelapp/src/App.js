02eee71df931e5345a0c43a2b0f282708b75e2da
sappy12dream <sapnaspy@gmail.com>
2020-09-01 09:59:26 +0530

    router

--- travelapp/src/App.js
+++ travelapp/src/App.js
@@ -14,7 +14,7 @@ function App() {
         <Route path="/" exact>
           <Home />
         </Route>
-        <Route path="/services">
+        <Route path="/places">
           <Places/>
         </Route>
       </Switch>
